a5f7b80f94398d381b411bed396719cb7646fa66
Mihail Costraci <mihai.costraci@feel-it-services.com>
2020-03-18 19:13:15 +0200

    Added: Sol key animation.

--- Circle-Completion-Animation/ViewController.swift
+++ Circle-Completion-Animation/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     var circleView: UIView!
     let circleOrigin = CGPoint(x: 50, y: 100)
-    let circleSize = CGSize(width: 300, height: 300)
+    let circleSize = CGSize(width: 30, height: 30)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,28 +23,13 @@ class ViewController: UIViewController {
         circleView.backgroundColor = .orange
         view.addSubview(circleView)
         
-        configureCircleLayer()
+        // Cheia sol animtion
+        startCheiaSolAnimation()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.startCircleAnimation()
-        }
-        
-//        setUpCircle()
+        // Circle animtion
+//        startCircleAnimation()
     }
-    
-    func setUpCircle() {
-        
-        let shapeLayer = CAShapeLayer()
-    
-        let path = UIBezierPath(arcCenter: CGPoint(x: circleSize.width/2, y: circleSize.height/2),
-                                radius: circleSize.width/2,
-                                startAngle: 10.toRadians(),
-                                endAngle: 180.toRadians(),
-                                clockwise: true)
-        
-        shapeLayer.path = path.cgPath
-        circleView.layer.mask = shapeLayer
-    }
+
     
     private var circleLayer = CAShapeLayer()
 
@@ -78,6 +63,8 @@ class ViewController: UIViewController {
 
     private func startCircleAnimation() {
         
+        configureCircleLayer()
+        
         circleLayer.strokeEnd = 1
         
         let animation = CABasicAnimation(keyPath: "strokeEnd")
@@ -88,6 +75,68 @@ class ViewController: UIViewController {
         circleLayer.add(animation, forKey: nil)
     }
 
+    func startCheiaSolAnimation() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.cheiaSol()
+        }
+    }
+    
+    func cheiaSol() {
+        
+        //// Bezier Drawing
+        let cheiaSolPath = UIBezierPath()
+        cheiaSolPath.move(to: CGPoint(x: 103.87, y: 70.21))
+        cheiaSolPath.addCurve(to: CGPoint(x: 110.5, y: 59.5), controlPoint1: CGPoint(x: 106.18, y: 70.14), controlPoint2: CGPoint(x: 108.77, y: 61.94))
+        cheiaSolPath.addCurve(to: CGPoint(x: 117.5, y: 56.5), controlPoint1: CGPoint(x: 112.52, y: 56.65), controlPoint2: CGPoint(x: 117.5, y: 56.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 126.5, y: 59.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 132.5, y: 70.21))
+        cheiaSolPath.addLine(to: CGPoint(x: 126.5, y: 84.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 117.5, y: 92.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 103.87, y: 92.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 88.5, y: 84.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 81.5, y: 72.89))
+        cheiaSolPath.addCurve(to: CGPoint(x: 88.5, y: 59.5), controlPoint1: CGPoint(x: 81.5, y: 72.89), controlPoint2: CGPoint(x: 84.26, y: 63.68))
+        cheiaSolPath.addCurve(to: CGPoint(x: 129.82, y: 23.85), controlPoint1: CGPoint(x: 102.23, y: 45.99), controlPoint2: CGPoint(x: 131.18, y: 23.85))
+        cheiaSolPath.addCurve(to: CGPoint(x: 126.5, y: 9.5), controlPoint1: CGPoint(x: 129.22, y: 23.85), controlPoint2: CGPoint(x: 130.23, y: 15.18))
+        cheiaSolPath.addCurve(to: CGPoint(x: 113.5, y: 2.5), controlPoint1: CGPoint(x: 123.49, y: 4.92), controlPoint2: CGPoint(x: 115.88, y: 3.19))
+        cheiaSolPath.addCurve(to: CGPoint(x: 104.5, y: 9.5), controlPoint1: CGPoint(x: 109.97, y: 1.48), controlPoint2: CGPoint(x: 104.5, y: 9.5))
+        cheiaSolPath.addLine(to: CGPoint(x: 104.5, y: 22.5))
+        UIColor.black.setStroke()
+        cheiaSolPath.lineWidth = 1
+        cheiaSolPath.stroke()
+
+
+        //// Bezier 2 Drawing
+        let bezier2Path = UIBezierPath()
+        bezier2Path.move(to: CGPoint(x: 104.5, y: 22.5))
+        bezier2Path.addLine(to: CGPoint(x: 104.5, y: 112.5))
+        UIColor.black.setStroke()
+        bezier2Path.lineWidth = 1
+        bezier2Path.stroke()
+        
+        cheiaSolPath.append(bezier2Path)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame.origin = CGPoint(x: 110.5, y: 59.5)
+        shapeLayer.strokeColor = UIColor.orange.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 2
+        shapeLayer.path = cheiaSolPath.cgPath
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        
+        let cheiaSolAnimation = CAKeyframeAnimation(keyPath: "position")
+        cheiaSolAnimation.isAdditive = true
+        cheiaSolAnimation.path = cheiaSolPath.cgPath
+        cheiaSolAnimation.duration = 3
+        cheiaSolAnimation.calculationMode = CAAnimationCalculationMode(rawValue: "paced")
+        
+            
+        circleView.layer.add(cheiaSolAnimation, forKey: "follow cheia sol")
+    }
+    
 }
 
 extension Int {
